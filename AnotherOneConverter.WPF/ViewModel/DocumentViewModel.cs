1c02573815a61b774eb704ef40fb400684b777f5
ivukovi4 <ivukovi4@gmail.com>
2016-06-06 18:06:21 +0500

    DocumentViewModel FileSystemWatcher was added.

--- AnotherOneConverter.WPF/ViewModel/DocumentViewModel.cs
+++ AnotherOneConverter.WPF/ViewModel/DocumentViewModel.cs
@@ -5,14 +5,30 @@ using System.IO;
 using System.Linq;
 
 namespace AnotherOneConverter.WPF.ViewModel {
-    public abstract class DocumentViewModel : ObservableObject {
+    public abstract class DocumentViewModel : ObservableObject, IDisposable {
+        private static readonly IDictionary<FileSystemWatcher, int> Watchers = new Dictionary<FileSystemWatcher, int>();
+
         public DocumentViewModel() { }
 
         public abstract IEnumerable<string> SupportedExtensions { get; }
 
         public abstract string ConvertToPdf(string targetDirectory);
 
-        protected FileInfo FileInfo { get; private set; }
+        protected FileSystemWatcher Watcher { get; private set; }
+
+        private FileInfo _fileInfo;
+        public FileInfo FileInfo {
+            get {
+                return _fileInfo;
+            }
+            private set {
+                if (Set(ref _fileInfo, value)) {
+                    RaisePropertyChanged(() => IsSupported);
+                    RaisePropertyChanged(() => LastWriteTime);
+                    RaisePropertyChanged(() => FileName);
+                }
+            }
+        }
 
         private string _filePath;
         public string FilePath {
@@ -22,6 +38,8 @@ namespace AnotherOneConverter.WPF.ViewModel {
             set {
                 if (Set(ref _filePath, value)) {
                     FileInfo = new FileInfo(_filePath);
+
+                    InvalidateWatcher();
                 }
             }
         }
@@ -42,6 +60,54 @@ namespace AnotherOneConverter.WPF.ViewModel {
             get {
                 return FileInfo.LastWriteTime;
             }
+        }
+
+        private FileSystemWatcher GetWatcher() {
+            var watcher = (from w in Watchers
+                           where string.Equals(w.Key.Path, FileInfo.DirectoryName, StringComparison.InvariantCultureIgnoreCase)
+                           select w.Key).FirstOrDefault();
+
+            if (watcher == null) {
+                watcher = new FileSystemWatcher(FileInfo.DirectoryName);
+
+                Watchers.Add(watcher, 1);
+            }
+            else {
+                Watchers[watcher]++;
+            }
+
+            return watcher;
+        }
+
+        private void InvalidateWatcher() {
+            DisposeWatcher();
+
+            Watcher = GetWatcher();
+            Watcher.Renamed -= OnRenamed;
+            Watcher.Renamed += OnRenamed;
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e) {
+            if (string.Equals(e.OldFullPath, _filePath, StringComparison.InvariantCultureIgnoreCase) == false)
+                return;
+
+            FilePath = e.FullPath;
+        }
+
+        private void DisposeWatcher() {
+            if (Watcher == null)
+                return;
+
+            if (--Watchers[Watcher] == 0) {
+                Watchers.Remove(Watcher);
+                Watcher.Dispose();
+            }
+
+            Watcher = null;
+        }
+
+        public void Dispose() {
+            DisposeWatcher();
         }
     }
 }
